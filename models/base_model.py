ed26285bdc29d9f0ab7b12974a5fbb37457b255c
helectron <helena.cortes@udea.edu.co>
2021-06-27 16:59:30 -0500

    Public instance attributes

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ Base class """
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import json
-
 
 class BaseModel:
     """ Base class model that contains the all methods and attributes for the some classes """
 
     def __init__(self, *args, **kwargs):
         """ Constructor to initialize the attribute with args and kwars. """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         value_format = '%Y-%m-%dT%H:%M:%S.%f'
 
         if kwargs:
