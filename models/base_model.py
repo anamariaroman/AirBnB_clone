dbbd3148a7d60d0c6af9d5b46f6caae78cdf4115
helectron <helena.cortes@udea.edu.co>
2021-06-28 16:17:33 -0500

    pep for the format

--- models/base_model.py
+++ models/base_model.py
@@ -6,24 +6,28 @@ import json
 
 
 class BaseModel:
-    """ Base class model that contains the all
-    methods and attributes for the some classes """
+    """ Base class model that defines all
+    methods and attributes for the other classes """
 
     def __init__(self):
-        """ Constructor to initialize the attribute with args and kwars. """
+        """ Constructor to initialize the attribute with args and kwars """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         value_format = '%Y-%m-%dT%H:%M:%S.%f'
 
     def __str__(self):
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+        """ Representation string of the information """
+        return "[{}] ({}) {}"\
+            .format(__class__.__name__, self.id, self.__dict__)
 
-"""def save(self):
- actualiza el atributo de instancia updated_at con datetime actual
+    def save(self):
+    "actualiza el atributo de instancia updated_at con datetime actual"
 
-def to_dict(self):
- retorna un diccionario que contiene todas las key/values de la
- instancia __dict__
- created_at y updated_at debesn convertirse a objeto de cadena en formato ISO:
-format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)"""
+    """
+    def to_dict(self):
+    retorna un diccionario que contiene todas las key/values de la
+    instancia __dict__
+    created_at y updated_at debesn convertirse a objeto de cadena en formato ISO:
+    format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)"""
+    """
