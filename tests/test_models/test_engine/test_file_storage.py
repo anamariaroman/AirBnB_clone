af4be8e714d25af96922b4cb912501f1856ee175
helectron <helena.cortes@udea.edu.co>
2021-06-30 22:32:52 -0500

    test file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Model Unittest """
+from models.base_model import BaseModel
+import unittest
+from models.engine.file_storage import FileStorage
+import json
+import pep8
+
+class TestFileStorage(unittest.TestCase):
+
+    def test_pep8(self):
+        """Test that models/base.py conforms to PEP8."""
+        pep8_style = pep8.StyleGuide(quiet=True)
+        result = pep8_style.check_files(["models/base_model.py"])
+        # unit testing to check the equality of two values
+        # assertEqual(firstValue, secondValue, message)
+        self.assertEqual(result.total_errors, 0, 
+                        "Found code style errors and warnings found.")
+
+    def test_docstring(self):
+        """ Test doc strings """
+        # assertIsNotNone(x): check that x is not None
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
